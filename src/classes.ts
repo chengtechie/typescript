03c580b5c4df94739081b57e4971fe06b9e57c04
chengchinlim <chengchin5227@gmail.com>
2020-10-11 19:49:43 -0700

    Test change remote url

--- src/classes.ts
+++ src/classes.ts
@@ -4,6 +4,10 @@ class Department {
     constructor(n: string) {
         this.name = n
     }
+
+    describe() { // object method
+
+    }
 }
 const accounting = new Department('Accounting')
 console.log(accounting)
@@ -12,5 +16,11 @@ class Product { // short cut
     private isListed: boolean
     constructor(public title: string, public price: number) {
         this.isListed = true
+    }
+}
+
+class ProductA extends Product {
+    constructor(price: number) {
+        super('A', price);
     }
 }
